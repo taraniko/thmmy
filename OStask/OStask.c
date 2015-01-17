76b2deeac484a195aaef6a32e93201804bd96e8f
Nick Taras <driverbulba@gmail.com>
2015-01-17 21:37:32 +0200

    Added cd, executions, but child dies

--- OStask/OStask.c
+++ OStask/OStask.c
@@ -6,8 +6,10 @@
 
 
 #define MAX_COMMAND_SIZE 100
-#define MAX_TOKEN_SIZE 20
+#define MAX_TOKEN_SIZE 50
 #define MAX_PARSED_TOKENS 5
+
+static pid_t pid;
 
 //void sig_handler(int signo)
 //{
@@ -17,6 +19,7 @@
 //  	exit(0);
 //}
 
+
 void  INThandler(int sig)
 {
 
@@ -24,6 +27,54 @@ void  INThandler(int sig)
      printf("\n\nUser asked for termination.\n");
      printf("The shell is now terminating...\n\n\n");
      exit(0);
+}
+
+
+
+int cd(char *pth){
+
+	int result;
+    char path[1000];
+    strcpy(path,pth);
+
+
+    char cwd[256];
+    getcwd(cwd,sizeof(cwd));
+
+    strcat(cwd,"/");
+    strcat(cwd,path);
+    result = chdir(cwd);
+
+    return result;
+  }
+
+
+void  execute(char **argv, int background)
+{
+     //pid_t  pid;
+
+	//TODO Fix Child dead issue
+
+     int    status;
+
+     if ((pid = fork()) < 0) {     /* fork a child process           */
+          printf("*** ERROR: forking child process failed\n");
+          exit(1);
+     }
+     else if (pid == 0) {          /* for the child process:         */
+          if (execvp(*argv, argv) < 0) {     /* execute the command  */
+               printf("*** ERROR: exec failed\n");
+               exit(1);
+          }
+     }
+     else {                                  /* for the parent:      */
+    	 if(background){
+    		 printf("\nProcess with pid [%d] started.",pid);
+    	 }
+    	 else{
+          while (wait(&status) != pid);       /* wait for completion  */
+    	 }
+     }
 }
 
 
@@ -39,17 +90,21 @@ int main(void)
     int char_count;
     int found_space;
     int active_tokens;
-    int error = 0;
+    int cd_result;
+    char cwd[256];
 
 
 
+
+    strcat(cwd,"/");
+    chdir(cwd);
     system("clear");
 
     //Main loop
     while(1){
 
-
-    	printf("OStaskShell:~$ ");
+    	getcwd(cwd,sizeof(cwd));
+    	printf("OStaskShell:%s$ ", cwd );
 
     	fgets (input, MAX_COMMAND_SIZE * sizeof(char) , stdin);
     	length = strlen(input);
@@ -94,22 +149,51 @@ int main(void)
     	for(i=0; i<MAX_PARSED_TOKENS; i++){
     		if (token_length[i] != 0){
     			active_tokens++;
-    			token[i][token_length[i]] = NULL;
+    			token[i][token_length[i]] =NULL;
     			token_length[i]++;
+    		}
+    	}
+    	token[active_tokens] = (char *)0;
+
+
+
+//    	printf("\n\nActive tokens: %d", active_tokens);
+//
+//    	for( i=0; i<active_tokens; i++){
+//    		printf("\nToken[%d] = %s",i,token[i]);
+//    	}
+//
+//    	for( i=0; i<MAX_PARSED_TOKENS; i++){
+//    	    printf("\nToken[%d] length = %d",i,token_length[i]);
+//    	}
+
+    	// check if command is "cd"
+    	if (!strcmp(token[0],"cd")){
+    		cd_result = cd(token[1]);
+    		if (cd_result == -1){
+    			printf("Cannot change to that directory");
+    		}
+    	}
+
+    	// check if command is exit
+    	else if (!strcmp(token[0],"exit")){
+    		exit(0);
+    	}
+
+    	else{
+    		//TODO & MUST BE REMOVED
+    		if(token[active_tokens-1][token_length[active_tokens-1]-2] == '&'){
+    			token[active_tokens-1][token_length[active_tokens-1]-2] = NULL;
+    			execute(token,1); //1 to execute in background
+    		}
+    		else{
+    			execute(token,0);
     		}
     	}
 
 
 
-    	printf("\n\nActive tokens: %d", active_tokens);
-
-    	for( i=0; i<active_tokens; i++){
-    		printf("\nToken[%d] = %s",i,token[i]);
-    	}
-
-    	for( i=0; i<MAX_PARSED_TOKENS; i++){
-    	    printf("\nToken[%d] length = %d",i,token_length[i]);
-    	}
+    	//
 
     	printf("\n");
     }
