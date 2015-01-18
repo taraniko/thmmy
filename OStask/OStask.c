15baf5e1ff6484b8973dcc7ab14d0992a218f04b
Nick Taras <driverbulba@gmail.com>
2015-01-18 21:59:31 +0200

    Working code. Needs documentation

--- OStask/OStask.c
+++ OStask/OStask.c
@@ -3,16 +3,14 @@
 #include <string.h>
 #include <signal.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 
 #define MAX_COMMAND_SIZE 100
 #define MAX_TOKEN_SIZE 50
 #define MAX_PARSED_TOKENS 5
-
-//static pid_t pid;
-
-//TODO fix ls & instead of ls&  ISSUE
-
+#define BACKGROUND 1
+#define FOREGROUND 0
 
 
 
@@ -45,29 +43,24 @@ int cd(char *pth){
   }
 
 
-void  execute(char **argv, int background)
+void execute(char **argv, int method)
 {
      pid_t  pid;
-
-
      int    status;
 
-     printf("\nToken[0] = %s\n", argv[0]);
-     printf("Token[1] = %s\n", argv[1]);
-
      if ((pid = fork()) < 0) {     /* fork a child process           */
-          printf("*** ERROR: forking child process failed\n");
+          printf("*** ERROR: Forking child process failed\n");
           exit(1);
      }
      else if (pid == 0) {          /* for the child process:         */
           if (execvp(*argv, argv) < 0) {     /* execute the command  */
-               printf("*** ERROR: exec failed\n");
+               printf("*** ERROR: Command not found or execution failed\n");
                exit(1);
           }
           exit(0);
      }
      else {                                  /* for the parent:      */
-    	 if(background){
+    	 if(method == BACKGROUND){
     		 printf("\nProcess with pid [%d] started.",pid);
     	 }
     	 else{
@@ -80,8 +73,11 @@ void  execute(char **argv, int background)
 int main(void)
 {
 	signal(SIGINT, INThandler);
+
     char *input = (char *)malloc(MAX_COMMAND_SIZE*sizeof(char));
     char *token[MAX_PARSED_TOKENS];
+    char cwd[256];
+
     int token_length[5];
     int i;
     int length = 0;
@@ -90,13 +86,8 @@ int main(void)
     int found_space;
     int active_tokens;
     int cd_result;
-    char cwd[256];
 
 
-
-
-    strcat(cwd,"/");
-    chdir(cwd);
     system("clear");
 
     //Main loop
@@ -114,19 +105,14 @@ int main(void)
         	token_length[i] = 0;
         }
 
-    	//TODO add support for & symbol
-
     	//parsing command and possible parameters
     	found_space = 1;
     	current_token_num = 0;
     	char_count = 0;
     	for (i = 0; i < length; i++) {
     		if (current_token_num>4){
-    			printf("\nOvernumbered parameters, only 4 where stored\n");
-
+    			printf("\n*** ERROR: Overnumbered parameters, only 4 where stored\n");
     			break;
-    			//current_token_num = 4;
-    			//found_space = 1;
     		}
     		if (input[i] == ' ') {
     			if (!found_space){      //if multiple spaces, then we ignore them
@@ -144,35 +130,23 @@ int main(void)
     	}
     	token_length[current_token_num]--;
 
+    	//find active tokens, add NULL to last parameter
     	active_tokens = 0;
     	for(i=0; i<MAX_PARSED_TOKENS; i++){
     		if (token_length[i] != 0){
     			active_tokens++;
-    			token[i][token_length[i]] =NULL;
+    			token[i][token_length[i]] = '\0';
     			token_length[i]++;
     		}
     	}
     	token[active_tokens] = (char *)0;
 
-        printf("\nToken[0] = %s\n", token[0]);
-        printf("Token[1] = %s\n", token[1]);
-        printf("Token[1] length = %d", token_length[1]);
-
-//    	printf("\n\nActive tokens: %d", active_tokens);
-//
-//    	for( i=0; i<active_tokens; i++){
-//    		printf("\nToken[%d] = %s",i,token[i]);
-//    	}
-//
-//    	for( i=0; i<MAX_PARSED_TOKENS; i++){
-//    	    printf("\nToken[%d] length = %d",i,token_length[i]);
-//    	}
 
     	// check if command is "cd"
     	if (!strcmp(token[0],"cd")){
     		cd_result = cd(token[1]);
     		if (cd_result == -1){
-    			printf("Cannot change to that directory");
+    			printf("*** ERROR: Cannot change to that directory\n");
     		}
     	}
 
@@ -181,16 +155,18 @@ int main(void)
     		exit(0);
     	}
 
+    	// when a binary needs to be executed
     	else{
-    		//TODO & MUST BE REMOVED
-    		if(token[active_tokens-1][token_length[active_tokens-1]-2] == '&'){
-    			printf("Mphka\n");
-    			token[active_tokens-1][token_length[active_tokens-1]-2] = '\0';
-    			printf("KAi to allaksa\n");
-    			execute(token,1); //1 to execute in background
+    		if(token[active_tokens-1][token_length[active_tokens-1]-2] == '&'){ //if last symbol is &
+
+    			token[active_tokens-1][token_length[active_tokens-1]-2] = '\0'; //make it NULL
+    			if(token_length[active_tokens-1] == 2){ // if the last token stores only & character
+    				token[active_tokens-1] = (char *)0; // make it the last argument by NULLing it.
+    			}
+    			execute(token,BACKGROUND); //1 to execute in background
     		}
     		else{
-    			execute(token,0);
+    			execute(token,FOREGROUND); //0 to execute in foreground
     		}
     	}
 
