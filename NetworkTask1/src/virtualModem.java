9d64e5c8972113f9715389cccbbc7deaf2ffee15
Nick Taras <driverbulba@gmail.com>
2015-01-19 23:41:18 +0200

    Added NetworkTask1 project

---
+++ NetworkTask1/src/virtualModem.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author nick
+ *
+ */
+public class virtualModem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
