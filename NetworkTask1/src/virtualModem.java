1c4c6d1d5f048deafda573ffb2e9e0d64065dbac
Nick Taras <driverbulba@gmail.com>
2015-01-20 02:54:48 +0200

    Added demo program for communicating with virtual modem

--- NetworkTask1/src/virtualModem.java
+++ NetworkTask1/src/virtualModem.java
@@ -1,20 +1,36 @@
-/**
- * 
- */
-
-/**
- * @author Taras Nikos
+/*
+ *
+ * Diktya Ypologistwn I
+ *
+ * Experimental Virtual Lab
+ *
+ * Java virtual modem communications seed code
  *
  */
+import ithakimodem.*;
+
 public class virtualModem {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("gamatooo2");
-
+	public static void main(String[] param) {
+		(new virtualModem()).rx();
 	}
 
+	public void rx() {
+		int k;
+		Modem modem;
+		modem = new Modem();
+		modem.setSpeed(1000);
+		modem.setTimeout(2000);
+		modem.open("ithaki");
+		for (;;) {
+			try {
+				k = modem.read();
+				if (k == -1)
+					break;
+				System.out.print((char) k);
+			} catch (Exception x) {
+				break;
+			}
+		}
+		modem.close();
+	}
 }
