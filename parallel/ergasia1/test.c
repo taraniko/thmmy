c7d40d5cdf4f550cd31b8559ab4ce85e57eec986
Nick Taras <driverbulba@gmail.com>
2015-11-15 10:51:14 +0200

    Final test.c missing write to file

--- parallel/ergasia1/test.c
+++ parallel/ergasia1/test.c
@@ -23,6 +23,7 @@
 /* 
 ------- ---------------------- 
    Nikos Pitsianis, Duke CS 
+   Taras Nikos, Auth Student
 -----------------------------
 
 */
@@ -42,19 +43,20 @@ double seq_time;
 
 pthread_mutex_t n_mutex;
 
+typedef struct{
+    int lo, cnt, dir;
+} targ;
 
 int N;        // data array size
 int *a;       // data array to be sorted
-int n;		  // number of threads	 
+int n;		    // number of threads	 
 
 
-int nThreads = 1;             
-int sThreads = 0;			  // DEBUG
-int threshold;        // TODO
+int nThreads = 1;          // number of active threads
+int threshold;             // quicksort is used below this threshold
 
 const int ASCENDING  = 1;
 const int DESCENDING = 0;
-
 
 void init(void);
 void print(void);
@@ -67,10 +69,12 @@ void impBitonicSort( void );
 void recBitonicSort( int lo, int cnt, int dir );
 void bitonicMerge( int lo, int cnt, int dir );
 
-void Psort( void );
+void pThreadSort( void );
 void * pThreadBitonicSort( void * arg );
 void * pThreadBitonicMerge( void * arg );
-void PimpBitonicSort( void );
+
+void openmpBitonicSort( void );
+
 void cilkBitonicSort(void);
 
 
@@ -79,14 +83,7 @@ void cilkrecBitonicSort(int lo, int cnt, int dir);
 void cilkbitonicMerge(int lo, int cnt, int dir);
 
 
-/** compare for qsort **/
-int desc( const void *a, const void *b ){
-    int* arg1 = (int *)a;
-    int* arg2 = (int *)b;
-    if( *arg1 > *arg2 ) return -1;
-    else if( *arg1 == *arg2 ) return 0;
-    return 1;
-}
+/* quicksort compare functions */
 int asc( const void *a, const void *b ){
     int* arg1 = (int *)a;
     int* arg2 = (int *)b;
@@ -95,79 +92,86 @@ int asc( const void *a, const void *b ){
     return 1;
 }
 
-/** the main program **/ 
+int desc( const void *a, const void *b ){
+    int* arg1 = (int *)a;
+    int* arg2 = (int *)b;
+    if( *arg1 > *arg2 ) return -1;
+    else if( *arg1 == *arg2 ) return 0;
+    return 1;
+}
+
+
+/* program flow */ 
 int main( int argc, char **argv ) {
 
     if (argc != 3 || atoi( argv[ 2 ] ) > 256 ) {
-        printf( "Usage: %s q t\n  where n=2^q is problem size (power of two), and t is the number of threads, <=256, to use.\n", argv[ 0 ] );
+        printf( "Usage: %s q p\n  where N=2^q is problem size (power of two), and 2^p is the number of threads, <=256.\n", argv[ 0 ] );
         exit( 1 );
     }
 
-    N = 1 << atoi( argv[ 1 ] );
+    N = 1 << atoi( argv[ 1 ] ); 
     n = 1 << atoi( argv[ 2 ] );
 
     threshold = N/n;
 
-    const char* wow;
-    char n_threads_char = (char) n;
-    const char* char_pointer;
-    char_pointer = &n_threads_char;
 
-
+    // set max number of cilk threads
     char str[5];
     sprintf(str, "%d", n);
     __cilkrts_set_param("nworkers", str);
 
-
+    // initialize pthread mutex
     pthread_mutex_init(&n_mutex, NULL);
     
- 
+    // allocate memory for a array
     a = (int *) malloc( N * sizeof( int ) );
+    
+    // serial quicksort
     init();
     gettimeofday( &startwtime, NULL );
     qsort( a, N, sizeof( int ), asc );
     gettimeofday( &endwtime, NULL );
     seq_time = (double)( ( endwtime.tv_usec - startwtime.tv_usec ) / 1.0e6 + endwtime.tv_sec - startwtime.tv_sec );
     printf( "Qsort time = %f\n", seq_time );
-//    test();
+    test();
    
-    //init();
-    //gettimeofday( &startwtime, NULL );
-    //sort();
-    //gettimeofday( &endwtime, NULL );
-    //seq_time = (double)( ( endwtime.tv_usec - startwtime.tv_usec ) / 1.0e6 + endwtime.tv_sec - startwtime.tv_sec );
-    //printf( "Bitonic serial recursive wall clock time = %f\n", seq_time );
-  //  test();
-     init();
+    // bitonic serial recursive
+    init();
     gettimeofday( &startwtime, NULL );
-    Psort();
+    sort();
+    gettimeofday( &endwtime, NULL );
+    seq_time = (double)( ( endwtime.tv_usec - startwtime.tv_usec ) / 1.0e6 + endwtime.tv_sec - startwtime.tv_sec );
+    printf( "Bitonic serial recursive time = %f\n", seq_time );
+    test();
+
+    // bitonic pthreads recursive
+    init();
+    gettimeofday( &startwtime, NULL );
+    pThreadSort();
     gettimeofday( &endwtime, NULL );
     seq_time = (double)( ( endwtime.tv_usec - startwtime.tv_usec ) / 1.0e6 + endwtime.tv_sec - startwtime.tv_sec );
     printf( "Bitonic pthreads with %i threads time = %f\n", 1 << atoi( argv[ 2 ] ), seq_time );
     test();
-    //init();
-    //gettimeofday( &startwtime, NULL );
-    //impBitonicSort();
-    //gettimeofday( &endwtime, NULL );
-    //seq_time = (double)( ( endwtime.tv_usec - startwtime.tv_usec ) / 1.0e6 + endwtime.tv_sec - startwtime.tv_sec );
-    //printf( "Bitonic serial imperative wall clock time = %f\n", seq_time );
-    //test();
+
+    // bitonic serial imperative
     init();
     gettimeofday( &startwtime, NULL );
-    PimpBitonicSort();
+    impBitonicSort();
+    gettimeofday( &endwtime, NULL );
+    seq_time = (double)( ( endwtime.tv_usec - startwtime.tv_usec ) / 1.0e6 + endwtime.tv_sec - startwtime.tv_sec );
+    printf( "Bitonic serial imperative time = %f\n", seq_time );
+    test();
+    
+    // bitonic openmp imperative 
+    init();
+    gettimeofday( &startwtime, NULL );
+    openmpBitonicSort();
     gettimeofday( &endwtime, NULL );
     seq_time = (double)( ( endwtime.tv_usec - startwtime.tv_usec ) / 1.0e6 + endwtime.tv_sec - startwtime.tv_sec );
     printf( "Bitonic omp with %i threads time = %f\n", 1 << atoi( argv[ 2 ] ),  seq_time );
     test();
 
-    init();
-    gettimeofday( &startwtime, NULL );
-    cilkBitonicSort();
-    gettimeofday( &endwtime, NULL );
-    seq_time = (double)( ( endwtime.tv_usec - startwtime.tv_usec ) / 1.0e6 + endwtime.tv_sec - startwtime.tv_sec );
-    printf( "Bitonic cilk with %i threads time = %f\n", 1 << atoi( argv[ 2 ] ),  seq_time );
-    test();
-
+    // bitonic cilk recursive
     init();
     gettimeofday( &startwtime, NULL );
     cilksort();
@@ -175,20 +179,13 @@ int main( int argc, char **argv ) {
     seq_time = (double)( ( endwtime.tv_usec - startwtime.tv_usec ) / 1.0e6 + endwtime.tv_sec - startwtime.tv_sec );
     printf( "Bitonic cilk recursive with %i threads time = %f\n", 1 << atoi( argv[ 2 ] ),  seq_time );
     test();
-    
-    printf("Spawned: %d\n",sThreads);
-    printf("Cilk used %d threads\n",__cilkrts_get_nworkers());
 
 
 
 }
 
-/** -------------- SUB-PROCEDURES  ----------------- **/ 
 
-
-
-
-/** procedure test() : verify sort results **/
+/* procedure test() : verify sort results */
 void test() {
   int pass = 1;
   int i;
@@ -204,7 +201,7 @@ void test() {
 void init() {
   int i;
   for (i = 0; i < N; i++) {
-    a[i] = rand() % N; // (N - i);
+    a[i] = rand() % N; 
   }
 }
 
@@ -306,26 +303,24 @@ void impBitonicSort() {
 }
 
 
-typedef struct{
-    int lo, cnt, dir;
-} sarg;
-
-/** Procedure bitonicMerge
- *  Same as serial, but uses pthreads.
+/** Function pThreadBitonicMerge
+ *  If active threads < max threads the function spawns a new thread to split the work
  **/
 void * pThreadBitonicMerge( void * arg ){
-    int lo = ( ( sarg * ) arg ) -> lo;
-    int cnt = ( ( sarg * ) arg ) -> cnt;
-    int dir = ( ( sarg * ) arg ) -> dir;
+    int lo = ((targ*)arg) -> lo;
+    int cnt = ((targ*)arg) -> cnt;
+    int dir = ((targ*)arg) -> dir;
     if( cnt > 1 ){
         int k = cnt / 2;
         int i;
         for( i = lo; i < lo + k; ++i ){
             compare( i, i + k, dir );
         }
-        int t1 = 0, t2 = 0;
-        sarg arg1, arg2;
-        pthread_t thread1, thread2;
+        int t1 = 0; // has spawned thread? default = 0
+        targ arg1,arg2;
+
+        pthread_t thread1;
+        
         arg1.lo = lo;
         arg1.cnt = k;
         arg1.dir = dir;
@@ -334,47 +329,26 @@ void * pThreadBitonicMerge( void * arg ){
         arg2.cnt = k;
         arg2.dir = dir;
       
-        
         if (nThreads<n){  
           pthread_create( &thread1, NULL, pThreadBitonicMerge, &arg1 );
           pthread_mutex_lock(&n_mutex);
           nThreads++;
-          sThreads++;
           pthread_mutex_unlock(&n_mutex);
-          t1 = 1;
+          t1 = 1; // a thread has been spawned
         }
         else
-        
-          //bitonicMerge(lo, k, dir);
           pThreadBitonicMerge(&arg1);
 
-      	/*
-        if (nThreads<n){  
-          pthread_create( &thread2, NULL, pThreadBitonicMerge, &arg2 );
-          pthread_mutex_lock(&n_mutex);
-          nThreads++;
-          sThreads++;
-          pthread_mutex_unlock(&n_mutex);
-          t2 = 1;
-        }
-        else
-        */
-          //bitonicMerge(lo+k, k, dir);
-          pThreadBitonicMerge(&arg2);
+        
+        pThreadBitonicMerge(&arg2);
 
         if (t1){
-              pthread_join( thread1, NULL );
-              pthread_mutex_lock(&n_mutex);
-              nThreads--;
-              pthread_mutex_unlock(&n_mutex);
-            }
-            if (t2){
-              pthread_join( thread2, NULL );
-              pthread_mutex_lock(&n_mutex);
-              nThreads--;
-              pthread_mutex_unlock(&n_mutex);
-            }
-        
+          pthread_join( thread1, NULL );
+          pthread_mutex_lock(&n_mutex);
+          nThreads--;
+          pthread_mutex_unlock(&n_mutex);
+        }
+
     }
     return 0;
 }
@@ -383,62 +357,49 @@ void * pThreadBitonicMerge( void * arg ){
     first produces a bitonic sequence by recursively sorting 
     its two halves in opposite sorting orders, and then
     calls bitonicMerge to make them in the same order 
-
-    Uses pthreads
+    Spawns a new thread if active threads are less than max thread number
+    
  **/
 
 void * pThreadBitonicSort( void * arg ){
-    int lo = ( ( sarg * ) arg ) -> lo;
-    int cnt = ( ( sarg * ) arg ) -> cnt;
-    int dir = ( ( sarg * ) arg ) -> dir;
+    int lo = ((targ*)arg) -> lo;
+    int cnt = ((targ*)arg) -> cnt;
+    int dir = ((targ*)arg) -> dir;
 
     if ( cnt > 1 ) {
         int k = cnt / 2;
-        if( cnt<=threshold ) {
-            qsort( a + lo, k, sizeof( int ), asc );
-            qsort( a + ( lo + k ) , k, sizeof( int ), desc );  // TODO SPAWN THREAD FOR QUICKSORT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        if( cnt<=threshold ) {  // if array size is less or equal to N/n
+            qsort( a + lo, k, sizeof( int ), asc );  // use quicksort 
+            qsort( a + ( lo + k ) , k, sizeof( int ), desc );  
         }
         else{
 
-            int t1 = 0, t2 = 0;
-            sarg arg1;
+            int t1 = 0;
+            
+            targ arg1, arg2;
+
             pthread_t thread1;
+            
             arg1.lo = lo;
             arg1.cnt = k;
             arg1.dir = ASCENDING;
+
+            arg2.lo = lo + k;
+            arg2.cnt = k;
+            arg2.dir = DESCENDING;
             
             if (nThreads<n){  
               pthread_create( &thread1, NULL, pThreadBitonicSort, &arg1 );
               pthread_mutex_lock(&n_mutex);
               nThreads++;
-              sThreads++;
               pthread_mutex_unlock(&n_mutex);
               t1 = 1;
             }
             else
+            	pThreadBitonicSort(&arg1);
             
-              //recBitonicSort(lo, k, ASCENDING);
-            	//pThreadBitonicSort(&arg1);
-            	qsort(a+lo,k,sizeof(int),asc);
             
-            sarg arg2;
-            pthread_t thread2;
-            arg2.lo = lo + k;
-            arg2.cnt = k;
-            arg2.dir = DESCENDING;
-           /* 
-            if (nThreads<n){  
-              pthread_create( &thread2, NULL, pThreadBitonicSort, &arg2 );
-              pthread_mutex_lock(&n_mutex);
-              nThreads++;
-              sThreads++;
-              pthread_mutex_unlock(&n_mutex);
-              t2 = 1;
-            }
-            else
-            */
-              //recBitonicSort(lo+k, k, DESCENDING);
-              pThreadBitonicSort(&arg2);
+            pThreadBitonicSort(&arg2);
 
             if (t1){
               pthread_join( thread1, NULL );
@@ -446,31 +407,24 @@ void * pThreadBitonicSort( void * arg ){
               nThreads--;
               pthread_mutex_unlock(&n_mutex);
             }
-            if (t2){
-              pthread_join( thread2, NULL );
-              pthread_mutex_lock(&n_mutex);
-              nThreads--;
-              pthread_mutex_unlock(&n_mutex);
-            }
         }
-            sarg arg3;
-            arg3.lo = lo;
-            arg3.cnt = cnt;
-            arg3.dir = dir;
-            
 
-            pThreadBitonicMerge( &arg3 );
+        targ arg3;
+        arg3.lo = lo;
+        arg3.cnt = cnt;
+        arg3.dir = dir;
+            
+        pThreadBitonicMerge( &arg3 );
         
     }
     return 0;
 }
 
-/** function sort() 
-   Caller of recBitonicSort for sorting the entire array of length N 
-   in ASCENDING order
+/** function pThreadSort() 
+   Calls pThreadBitonicSort to sort the array in ASCENDING order
  **/
-void Psort() {
-    sarg arg;
+void pThreadSort() {
+    targ arg;
     arg.lo = 0;
     arg.cnt = N;
     arg.dir = ASCENDING;
@@ -481,59 +435,35 @@ void Psort() {
 
 
 /*
-  imperative version of bitonic sort
+  imperative version of bitonic sort using omenmp
+
 */
-void PimpBitonicSort() {
-    omp_set_num_threads( n );//"n" is the number of threads - arg[2]; 
-    int i,j,k=0,term;
-    //term = (int) log2( N );
+void openmpBitonicSort() {
+    omp_set_num_threads( n ); 
+    int i,j,k=0;
     for (k = 2; k <= N; k *= 2 ) {
         for (j=k>>1; j>0; j=j>>1) {
             #pragma omp parallel for
-                for (i=0; i<N; i++) {
-                    int ij=i^j;
-                    if ((ij)>i) {
-                        if ((i&k)==0 && a[i] > a[ij]) {
-                            exchange(i,ij);
-                        }
-                        if ((i&k)!=0 && a[i] < a[ij]){
-                            exchange(i,ij);
-                        }
+            for (i=0; i<N; i++) {
+                int ij=i^j;
+                if ((ij)>i) {
+                    if ((i&k)==0 && a[i] > a[ij]) {
+                        exchange(i,ij);
+                    }
+                    if ((i&k)!=0 && a[i] < a[ij]){
+                        exchange(i,ij);
                     }
                 }
+            }
         }
     }
 } 
 
-/*
-  imperative version of bitonic sort
-*/
-void cilkBitonicSort() { 
-    int i,j,k=0,term;
-    for (k = 2; k <= N; k *= 2 ) {
-        for (j=k>>1; j>0; j=j>>1) {
-                cilk_for (i=0; i<N; i++) {
-                    int ij=i^j;
-                    if ((ij)>i) {
-                        if ((i&k)==0 && a[i] > a[ij]) {
-                            exchange(i,ij);
-                        }
-                        if ((i&k)!=0 && a[i] < a[ij]){
-                            exchange(i,ij);
-                        }
-                    }
-                }
-        }
-    }
-}
 
 /*new cilk recursive */
 
-/** Procedure cilkbitonicMerge() 
-   It recursively sorts a bitonic sequence in ascending order, 
-   if dir = ASCENDING, and in descending order otherwise. 
-   The sequence to be sorted starts at index position lo,
-   the parameter cbt is the number of elements to be sorted. 
+/** function cilkbitonicMerge() 
+    Uses cilk threads 
  **/
 void cilkbitonicMerge(int lo, int cnt, int dir) {
   if (cnt>1) {
@@ -543,20 +473,19 @@ void cilkbitonicMerge(int lo, int cnt, int dir) {
       compare(i, i+k, dir);
     cilk_spawn cilkbitonicMerge(lo, k, dir);
     cilkbitonicMerge(lo+k, k, dir);
+    cilk_sync;
   }
 }
 
 
 
 /** function cilkrecBitonicSort() 
-    first produces a bitonic sequence by recursively sorting 
-    its two halves in opposite sorting orders, and then
-    calls bitonicMerge to make them in the same order 
+    uses cilk threads. If array size <= threshold, qsort is used. 
  **/
 void cilkrecBitonicSort(int lo, int cnt, int dir) {
   if (cnt>1) {
   	int k=cnt/2;
-  	if(cnt<threshold){
+  	if(cnt<=threshold){
     	cilk_spawn cilkrecBitonicSort(lo, k, ASCENDING);
     	cilkrecBitonicSort(lo+k, k, DESCENDING);
     	cilk_sync;
@@ -572,9 +501,9 @@ void cilkrecBitonicSort(int lo, int cnt, int dir) {
 
 
 /** function cilksort() 
-   Caller of recBitonicSort for sorting the entire array of length N 
-   in ASCENDING order
+   Calls of recBitonicSort to sort the array in ASCENDING order
  **/
 void cilksort() {
   cilkrecBitonicSort(0, N, ASCENDING);
 }
+
