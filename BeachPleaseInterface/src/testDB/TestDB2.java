804478a5fa09fac64e7affea83e82552df33acf7
Nick Taras <driverbulba@gmail.com>
2016-03-08 16:10:19 +0200

    Added delete and insert in admin

--- BeachPleaseInterface/src/testDB/TestDB2.java
+++ BeachPleaseInterface/src/testDB/TestDB2.java
@@ -3,7 +3,9 @@ package testDB;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
@@ -15,6 +17,7 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.swing.*;
 
@@ -58,7 +61,6 @@ class TestDB2 {
 	private void createAndShowGUI(){
 		
 		//Create and set up the window.
-		frame = new JFrame();
 		frame = new JFrame();
 		frame.setBounds(100, 100, 320, 240);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -128,6 +130,7 @@ class TestDB2 {
             //if username and password is good hide child window
             String username = user.getText();
             String password = pass.getText();
+            priviledge = new String ("");
             
             if ((username.equals("admin")) && (password.equals("admin")))
             {
@@ -154,12 +157,149 @@ class TestDB2 {
         }
 	}
 	
-	public void fillMainAdminCard(JPanel cardMain)
-	{
-		frame.setBounds(100, 100, 640, 480);
-		JTextArea textArea = new JTextArea(25,25);
-	    cardMain.add(textArea);
-	    textArea.setText("Admin Card");
+	public void fillMainAdminCard(final JPanel cardMain)
+	{   
+	    //frame.setBounds(100, 100, 640, 640);
+		cardMain.setLayout(new GridBagLayout());
+	    final GridBagConstraints c = new GridBagConstraints();
+		
+		int maxCols = 30;
+	    
+	    JLabel lblDeleteAcc = new JLabel("Διαγραφή ξενοδοχείου/ενοικιαζόμενων δωματίων");
+	    
+	    JLabel lblAcc = new JLabel("Ονομασία επιχείρησης:");
+	    final JComboBox<String> listAcc = new JComboBox<String>(db.getAccommodations());
+	    JButton btnDelete = new JButton("Διαγραφή");
+	    
+	    btnDelete.addActionListener(new ActionListener() {
+	    	 
+            public void actionPerformed(ActionEvent e)
+            {
+                int res = db.deleteAccommodation(listAcc.getSelectedItem().toString());
+                if (res == 0)
+                {
+                	listAcc.remove(listAcc.getSelectedIndex());
+                	DefaultComboBoxModel<String> model = (DefaultComboBoxModel<String>) listAcc.getModel();
+                	model.removeElementAt(listAcc.getSelectedIndex());
+                	listAcc.setModel(model);
+                	cardMain.repaint();
+                }
+            }
+        });     
+	    
+	    JLabel lblInsertAcc = new JLabel("Εισαγωγή ξενοδοχείου/ενοικιαζόμενων δωματίων");
+	    
+	    JLabel lblName = new JLabel("Ονομασία επιχείρησης:");
+	    final JTextField txtName = new JTextField(maxCols);
+	    
+	    JLabel lblAddress = new JLabel("Διεύθυνση:");
+	    final JTextField txtAddress = new JTextField(maxCols);
+	    
+	    JLabel lblBeach = new JLabel("Κοντινή παραλία:");
+	    final JComboBox<String>  listBeaches = new JComboBox<String>(db.getBeaches());
+	    JButton btnInsert = new JButton("Εισαγωγή");
+	    
+	    btnInsert.addActionListener(new ActionListener() {
+	    	 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+            	int res = db.insertAccommodation(txtName.getText(), txtAddress.getText(),
+            			listBeaches.getSelectedItem().toString());
+            	if (res == 0)
+                {
+                	DefaultComboBoxModel<String> model = (DefaultComboBoxModel<String>) listAcc.getModel();
+                	model.addElement(txtName.getText());
+                	listAcc.setModel(model);
+                	cardMain.repaint();
+                }
+            }
+        });     
+	    
+	    JLabel lblChangeMayor = new JLabel("Αλλαγή Δημάρχου");
+	    
+	    JLabel lblMunicipality = new JLabel("Δήμος:");
+	    JComboBox<String> listMunicipalities = new JComboBox<String>(db.getMunicipalities());
+	    
+	    JLabel lblNewMayor = new JLabel("Νέος Δήμαρχος:");
+	    JTextField txtNewMayor = new JTextField(maxCols);
+	    JButton btnModify = new JButton("Αλλαγή");
+	    
+	    btnModify.addActionListener(new ActionListener() {
+	    	 
+            public void actionPerformed(ActionEvent e)
+            {
+                //Execute when button is pressed
+            }
+        });     
+	        
+	    
+	    
+	    
+	    
+	    c.gridx = 0;
+	    c.gridy = 0;
+	    cardMain.add(lblDeleteAcc,c);
+	    
+	    c.gridy = 1;
+	    c.gridx = 0;
+	    cardMain.add(lblAcc,c);
+	    c.gridx++;
+	    cardMain.add(listAcc,c);
+	    c.gridx++;
+	    cardMain.add(btnDelete,c);
+	    
+	    //Insert Accommodation
+	    c.gridy = 2;
+	    c.gridx = 0;
+	    cardMain.add(lblInsertAcc,c);
+	    
+	    c.gridy = 3;
+	    c.gridx = 0;
+	    cardMain.add(lblName,c);
+	    c.gridx++;
+	    cardMain.add(txtName,c);
+	    
+	    c.gridy = 4;
+	    c.gridx = 0;
+	    cardMain.add(lblAddress,c);
+	    c.gridx++;
+	    cardMain.add(txtAddress,c);
+	    
+	    c.gridy = 5;
+	    c.gridx = 0;
+	    cardMain.add(lblBeach,c);
+	    c.gridx++;
+	    cardMain.add(listBeaches,c);
+	    
+	    c.gridy = 6;
+	    c.gridx = 2;
+	    cardMain.add(btnInsert,c);
+	    
+	    //Change Mayor
+	    c.gridy = 7;
+	    c.gridx = 0;
+	    cardMain.add(lblChangeMayor,c);
+	    
+	    c.gridy = 8;
+	    c.gridx = 0;
+	    cardMain.add(lblMunicipality,c);
+	    c.gridx++;
+	    cardMain.add(listMunicipalities,c);
+	    
+	    c.gridy = 9;
+	    c.gridx = 0;
+	    cardMain.add(lblNewMayor,c);
+	    c.gridx++;
+	    cardMain.add(txtNewMayor,c);
+	    
+	    c.gridy = 10;
+	    c.gridx = 2;
+	    cardMain.add(btnModify,c);
+	    
+	    frame.pack();
+	    
+	  
 	}
 	
 	public void fillMainUserCard(JPanel cardMain)
@@ -181,6 +321,8 @@ class TestDB2 {
 	    
 	    
 	    final JTextArea txtResult = new JTextArea();
+	    Font font = new Font("Serif", Font.BOLD, 18);
+        txtResult.setFont(font);
 	    
 	    JButton btnSearch = new JButton("Αναζήτηση");
 	    btnSearch.addActionListener(new ActionListener() {
@@ -236,6 +378,8 @@ class TestDB2 {
 	    c.anchor = GridBagConstraints.CENTER;
 	    cardMain.add(txtResult,c);
 	    
+	    frame.pack();
+	    
 	    
 	    
 	    
@@ -256,7 +400,7 @@ class ConnectDB
 			String userName = "nikos";
 			String password = "taras";
 
-			String url ="jdbc:sqlserver://37.6.143.67:1433;databaseName=stelios";
+			String url ="jdbc:sqlserver://nikolaspc.ddns.net:1433;databaseName=stelios";
 		
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
@@ -282,8 +426,9 @@ class ConnectDB
 				System.out.println(lastName);
 			}
 		} catch (SQLException e) {
-			System.err.println("Got an exception! ");
-			System.err.println(e.getMessage());
+			//System.err.println("Got an exception! ");
+			//System.err.println(e.getMessage());
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 		
@@ -302,13 +447,167 @@ class ConnectDB
 		}
 		catch(SQLException e)
 			{
-			System.err.println("Got an exception! ");
-			System.err.println(e.getMessage());
+			//System.err.println("Got an exception! ");
+			//System.err.println(e.getMessage());
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+
 			}
 		
 		String[] dArray = districts.toArray(new String[districts.size()]);
 		return dArray;
 	}
+	
+	public String[] getBeaches()
+	{
+		ArrayList<String> beaches = new ArrayList<String>();
+		beaches.add("None");
+		try {
+			ResultSet rs;
+			String q = new String("SELECT * FROM Beach");
+			rs = stmt.executeQuery(q);
+			while (rs.next()){
+				String beach = rs.getString("Name");
+				beaches.add(beach);
+			}
+		}
+		catch(SQLException e)
+			{
+			//System.err.println("Got an exception! ");
+			//System.err.println(e.getMessage());
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+
+			}
+		Collections.sort(beaches);
+		
+		String[] bArray = beaches.toArray(new String[beaches.size()]);
+		return bArray;
+	}
+	
+	public String[] getAccommodations()
+	{
+		ArrayList<String> accs = new ArrayList<String>();
+		try {
+			ResultSet rs;
+			String q = new String("SELECT * FROM Accommodation");
+			rs = stmt.executeQuery(q);
+			while (rs.next()){
+				String acc = rs.getString("Name");
+				accs.add(acc);
+			}
+		}
+		catch(SQLException e)
+			{
+			//System.err.println("Got an exception! ");
+			//System.err.println(e.getMessage());
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+
+			}
+		Collections.sort(accs);
+		String[] aArray = accs.toArray(new String[accs.size()]);
+		return aArray;
+	}
+	
+	public String[] getMunicipalities()
+	{
+		ArrayList<String> muns = new ArrayList<String>();
+		try {
+			ResultSet rs;
+			String q = new String("SELECT * FROM Municipality");
+			rs = stmt.executeQuery(q);
+			while (rs.next()){
+				String mun = rs.getString("Name");
+				muns.add(mun);
+			}
+		}
+		catch(SQLException e)
+			{
+			//System.err.println("Got an exception! ");
+			//System.err.println(e.getMessage());
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+
+			}
+		Collections.sort(muns);
+		String[] aArray = muns.toArray(new String[muns.size()]);
+		return aArray;
+	}
+	
+	public int deleteAccommodation(String acc)
+	{
+		boolean ok = true;
+		try {
+			String q = new String("DELETE FROM Accommodation WHERE Name='");
+			q += acc + "'";
+			stmt.executeUpdate(q);
+		}
+		catch(SQLException e)
+			{
+			//System.err.println("Got an exception! ");
+			//System.err.println(e.getMessage());
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			ok = false;
+			}
+		if (ok)
+		{
+			String result = "Η επιχείρηση με ονομασία " + acc + " έχει διαγραφεί επιτυχώς!";
+			JOptionPane.showMessageDialog(null, result, "Επιτυχία!", JOptionPane.INFORMATION_MESSAGE);
+			return 0;
+		}
+		return -1;
+	}
+	
+	public int getBeachIdFromName(String name)
+	{
+		int beachId = -1;
+		try {
+			ResultSet rs;
+			String q = new String("SELECT Id FROM Beach WHERE Name='");
+			q += name +"'";
+			rs = stmt.executeQuery(q);
+			while (rs.next()){
+				beachId = rs.getInt("Id");
+			}
+		}
+		catch(SQLException e)
+			{
+			//System.err.println("Got an exception! ");
+			//System.err.println(e.getMessage());
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+
+			}
+		return beachId;
+	}
+	
+	public int insertAccommodation(String name, String address, String beach)
+	{
+		boolean ok = true;
+		int beachId = getBeachIdFromName(beach);
+		try {
+			String q = new String("INSERT INTO Accommodation VALUES");
+			if (beachId < 0){
+				JOptionPane.showMessageDialog(null, "Δεν επιλέχθηκε παραλία.", "Προσοχή!", JOptionPane.INFORMATION_MESSAGE);
+				q += " ('"+ address + "','" + name + "',null,null,null,null,null,null)";
+			}
+			else
+				q += " ('"+ address + "','" + name + "',null,null," + beachId + ",null,null,null)";
+			stmt.executeUpdate(q);
+		}
+		catch(SQLException e)
+			{
+			//System.err.println("Got an exception! ");
+			//System.err.println(e.getMessage());
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			ok = false;
+			}
+		if (ok)
+		{
+			String result = "Η επιχείρηση με ονομασία " + name + " έχει καταχωρηθεί επιτυχώς!";
+			JOptionPane.showMessageDialog(null, result, "Επιτυχία!", JOptionPane.INFORMATION_MESSAGE);
+			return 0;
+		}
+		return -1;
+	}
+	
+	
 	
 	public String searchBeaches(String district, String popularity, boolean shower,
 			boolean lifeguard)
@@ -370,8 +669,10 @@ class ConnectDB
 				res += "Δεν υπάρχουν παραλίες με αυτά τα κριτήρια!";
 				
 		} catch (SQLException e) {
-			System.err.println("Got an exception! ");
-			System.err.println(e.getMessage());
+			//System.err.println("Got an exception! ");
+			//System.err.println(e.getMessage());
+			JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+
 		}
 		
 		return res;
